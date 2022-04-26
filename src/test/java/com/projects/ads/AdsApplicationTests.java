ce38adc711da8e1336d011f9ca0c643487b12875
Syntcha Fabiola Matou Soubong <syntcha-fabiola.matou-soubong@informatik.hs-fulda.de>
2022-04-26 23:39:11 +0200

    first publication

---
+++ src/test/java/com/projects/ads/AdsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projects.ads;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
