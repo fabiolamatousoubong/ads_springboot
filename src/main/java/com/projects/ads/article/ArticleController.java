0c0986b23ec3675d0f91d3b7930f9586500fbe7b
Christian Gabi <49684157+christian80gabi@users.noreply.github.com>
2022-05-10 00:15:35 +0100

    Revert "Update commit: index.html and articles.html"
    This reverts commit a8dac0594bf23ae0fad340ea4812fae480bd3c7c.

--- src/main/java/com/projects/ads/article/ArticleController.java
+++ src/main/java/com/projects/ads/article/ArticleController.java
@@ -1,105 +1,32 @@
 package com.projects.ads.article;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-
+@RestController
+@RequestMapping(path = "api/v1/article")
 public class ArticleController {
 
-    @RequestMapping(path = "articles")
-    abstract static class ArticleBaseController {
+    private final ArticleService articleService;
 
-        @RequestMapping
-        abstract Object getArticles();
-
-        @RequestMapping
-        abstract void addArticle(Article article);
-
-        @RequestMapping
-        abstract void updateArticle(Long articleId, Article article);
-
-        @RequestMapping
-        abstract void deleteArticle(Long articleId);
+    @Autowired
+    public ArticleController(ArticleService articleService) {
+        this.articleService = articleService;
     }
 
-    @RestController
-    @RequestMapping(path = "api/v1/articles")
-    static class ArticleRESTController extends ArticleBaseController {
-
-        private final ArticleService articleService;
-
-        @Autowired
-        public ArticleRESTController(ArticleService articleService) {
-            this.articleService = articleService;
-        }
-
-        @Override
-        @GetMapping
-        public List<Article> getArticles() {
-            return articleService.getArticles();
-        }
-
-        @Override
-        @PostMapping
-        public void addArticle(@RequestBody Article article) {
-            articleService.addArticle(article);
-        }
-
-        @Override
-        @PutMapping(path = "{articleId}")
-        public void updateArticle(@PathVariable("articleId") Long articleId, @RequestBody Article article) {
-            articleService.updateArticle(articleId, article);
-        }
-
-        @Override
-        @DeleteMapping(path = "{articleId}")
-        public void deleteArticle(@PathVariable("articleId") Long articleId) {
-            articleService.deleteArticle(articleId);
-        }
+    @GetMapping
+    public List<Article> getArticles() {
+        return articleService.getArticles();
     }
 
-    @Controller
-    @RequestMapping(path = "v1/articles")
-    static class ArticleHTMLController extends ArticleBaseController {
-
-        private final ArticleService articleService;
-
-        @Autowired
-        public ArticleHTMLController(ArticleService articleService) {
-            this.articleService = articleService;
-        }
-
-        @Override
-        @GetMapping
-        ModelAndView getArticles() {
-            ModelAndView model = new ModelAndView("articles");
-
-            model.addObject("title", "Articles");
-            model.addObject("articles", articleService.getArticles());
-
-            return model;
-        }
-
-        @Override
-        @PostMapping
-        public void addArticle(Article article) {
-            // TODO: Add instruction
-        }
-
-        @Override
-        @PutMapping
-        public void updateArticle(Long articleId, Article article) {
-            // TODO: Add instruction
-        }
-
-        @Override
-        @DeleteMapping
-        public void deleteArticle(Long articleId) {
-            // TODO: Add instruction
-        }
+    @PostMapping
+    public void addArticle(@RequestBody Article article) {
+        articleService.addArticle(article);
     }
 }
--- src/main/java/com/projects/ads/article/ArticleRepository.java
+++ src/main/java/com/projects/ads/article/ArticleRepository.java
@@ -3,12 +3,10 @@ package com.projects.ads.article;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ArticleRepository extends JpaRepository<Article, Long> {
-    @Query("SELECT s FROM Article s WHERE s.reference = ?1")
     Optional<Article> findArticleByReference(String reference);
     Optional<Article> findArticleByName(String name);
 }
